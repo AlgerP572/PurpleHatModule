c5ce2a5d1ab5d1646221ce2d8cdd0e8c2a1a32d5
Alger PIke <Alger_P@pacbell.net>
2025-12-08 20:42:03 -0800

    Latest version files.

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "377"
+  #define BUILD_NUMBER "378"
 #endif
 #ifndef VERSION
-  #define VERSION "v1.6.377 - 2025-12-06 19:27:39.490411"
+  #define VERSION "v1.6.378 - 2025-12-08 19:14:53.801867"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v1.6.377"
+  #define VERSION_SHORT "v1.6.378"
 #endif
